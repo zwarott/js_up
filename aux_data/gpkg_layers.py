d3126051749d086d000a0b63ddc3e953b4ded4fa
zwarott <zwarott@email.cz>
2024-08-01 22:27:43 +0200

    Add auxiliary data such as municipalities and content of gpkg file

---
+++ aux_data/gpkg_layers.py
@@ -0,0 +1,24 @@
+# Tables including layer styles and info about QGIS projects
+EXCLUDE_LAYERS = ["layer_styles", "qgis_projects"]
+
+# Standardized vector layers
+INCLUDE_LAYERS = [
+    "ReseneUzemi_p",
+    "UzemiPrvkyRP_p",
+    "ZastaveneUzemi_p",
+    "ZastavitelneUzemi_p",
+    "PlochyRZV_p",
+    "UzemniRezervy_p",
+    "KoridoryP_p",
+    "KoridoryN_p",
+    "Lokality_p",
+    "PlochyZmen_p",
+    "PlochyPodm_p",
+    "VpsVpoAs_p",
+    "VpsVpoAs_l",
+    "USES_p",
+    "SystemSidelniZelene_p",
+    "SystemVerProstr_p",
+    "PlochaVI_p",
+    "Zpochybneno_p",
+]
